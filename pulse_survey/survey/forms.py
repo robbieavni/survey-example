d8f15c37061757d871f645d010f95b87658b2d4e
Robbie Avni <robbieavni@gmail.com>
2023-06-15 10:51:08 +0100

    implements is_cabinet_office_email function

--- pulse_survey/survey/forms.py
+++ pulse_survey/survey/forms.py
@@ -4,10 +4,18 @@ from django.core.exceptions import ValidationError
 
 
 def is_cabinet_office_email(email_address):
-    # TODO - update to raise a ValidationError for non-Cabinet Office emails
+    # TODO consider instead inheriting from default email validator and overriding the domain checking part
+
+    if not email_address or "@" not in email_address:
+        raise ValidationError("Enter a Cabinet Office email address")
+
+    domain_part = email_address.rsplit("@", 1)[1]
+
+    if domain_part != "cabinetoffice.gov.uk":
+        raise ValidationError("Enter a Cabinet Office email address")
+
     return True
     
-
 
 class FeedbackForm(forms.Form):
     template_name = "feedback.html"
