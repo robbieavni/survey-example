1ceca5e1a8cee77ca1ff40c589a192da653440c4
Robbie Avni <robbieavni@gmail.com>
2023-06-15 11:27:28 +0100

    adds view tests for email validation

--- tests/test_views.py
+++ tests/test_views.py
@@ -11,3 +11,15 @@ class TestViews(TestCase):
         session_id = uuid.uuid4()
         response = self.client.get(f"/survey/{session_id}/location-question/")
         self.assertEqual(response.status_code, 200)
+
+class EmailFormViewTests(TestCase):
+    def test_valid_email(self):
+        form_data = {"email": "test@cabinetoffice.gov.uk", "content": "Test", "next": "Submit feedback"}
+        response = self.client.post("/survey/feedback/", data=form_data)
+        self.assertRedirects(response, '/survey/end/', status_code=302)
+
+    def test_invalid_email(self):
+        form_data = {"email": "test@gmail.com", "content": "Test", "next": "Submit feedback"}
+        response = self.client.post("/survey/feedback/", data=form_data)
+        self.assertIn(b"Enter a Cabinet Office email address", response.content)
+        self.assertEqual(response.status_code, 200)
